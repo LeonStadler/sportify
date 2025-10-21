91e7bc997d4655259bc97ab815a76cdc38ce0077
Leon <github@leon-stadler.com>
2025-10-21 19:48:09 +0200

    Add bcryptjs for password hashing

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 import pkg from 'pg';
 import { queueEmail } from './services/emailService.js';
 import {
